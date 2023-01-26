5479a77536565b4eadfde4ea79417ed8bac3c5d1
Mert Bingol <mertbingol00@protonmail.com>
2023-01-26 10:46:27 +0300

    Update description-hunter.py

--- description-hunter.py
+++ description-hunter.py
@@ -9,7 +9,7 @@ import re
 
 channell_id = input('Please enter Channel ID: ')
 url_part_1 = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId='
-url_part_2 = '&maxResults=50&key=***=json' #yildizli olan yere youtube api'nin key'i gelmelidir.
+url_part_2 = '&maxResults=50&key=AIzaSyD6xmCCs6zcZlQSILK1MN9oTsnGLIJ8mC8&alt=json'
 next_page_token = ''
 
 unparsing_url = url_part_1 + channell_id + url_part_2
